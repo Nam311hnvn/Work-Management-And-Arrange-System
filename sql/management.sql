3d8897f7e616e5ea1dc06bae27f97699f104994c
Nam311hnvn <zenkai311@gmail.com>
2021-10-24 21:07:04 +0700

    commit sql

---
+++ sql/management.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2021 at 04:06 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_calendar`
+--
+
+CREATE TABLE `tb_calendar` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `group_id` int(20) NOT NULL,
+  `cal_name` varchar(100) NOT NULL,
+  `create_on` date NOT NULL,
+  `date_time` datetime NOT NULL,
+  `note` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `user_nick` varchar(30) NOT NULL,
+  `user_pass` varchar(100) NOT NULL,
+  `user_level` int(11) DEFAULT NULL,
+  `user_status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_userinfo`
+--
+
+CREATE TABLE `tb_userinfo` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `user_dob` date NOT NULL,
+  `user_job` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_phone` int(20) NOT NULL,
+  `user_address` varchar(300) NOT NULL,
+  `group_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_calendar`
+--
+ALTER TABLE `tb_calendar`
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `tb_userinfo`
+--
+ALTER TABLE `tb_userinfo`
+  ADD UNIQUE KEY `UNIQUE` (`group_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_userinfo`
+--
+ALTER TABLE `tb_userinfo`
+  MODIFY `group_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_calendar`
+--
+ALTER TABLE `tb_calendar`
+  ADD CONSTRAINT `tb_calendar_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `tb_userinfo` (`group_id`),
+  ADD CONSTRAINT `tb_calendar_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`user_id`);
+
+--
+-- Constraints for table `tb_userinfo`
+--
+ALTER TABLE `tb_userinfo`
+  ADD CONSTRAINT `tb_userinfo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
