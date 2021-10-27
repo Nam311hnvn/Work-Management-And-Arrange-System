15adc338db755717391deb49c941ed56f7948283
PhongGT <megayako9@gmail.com>
2021-10-28 01:54:55 +0700

    Add login

---
+++ src/login.php
@@ -0,0 +1,44 @@
+<?php include('./reuse/header.php'); ?>
+<div class="container-fluid px-0">
+    <section class="vh-100" style="background-color: #eee;">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-75">
+                <div class=" col-md-5 col-lg-5">
+                    <div class="card text-black" style="border-radius: 25px;">
+                        <div class="card-body p-m-5 top-50">
+                            <div class="row justify-content-center">
+                                <div class="col-md-12">
+                                    <form class="mx-4" action="" method="post">
+                                        <h2 class="fw-bold text-center">Log in!</h2>
+                                        <div class="input-group flex-nowrap py-3">
+                                            <span class="input-group-text" id="addon-wrapping">Email/Username</span>
+                                            <input type="text" class="form-control" placeholder="Enter your Username"
+                                                aria-label="Username" aria-describedby="addon-wrapping">
+                                        </div>
+                                        <div class="input-group flex-nowrap pb-3">
+                                            <span class="input-group-text" id="addon-wrapping">Password</span>
+                                            <input type="text" class="form-control" placeholder="Enter your Password"
+                                                aria-label="Password" aria-describedby="addon-wrapping">
+                                        </div>
+                                        <div class="d-flex justify-content-start mb-3 mb-lg-4">
+                                            <a class="text-decoration-none" href="#">New around here? Sign up</a>
+                                        </div>
+                                        <div class="d-flex justify-content-start mb-3 mb-lg-4">
+                                            <button type="submit" class="btn btn-primary" name='Login'>Log In</button>
+                                        </div>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+
+
+<?php include('./reuse/footer.php'); ?>
