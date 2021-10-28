d0fe1498b592d502feef7eed97c1325fe01bf4d3
Nam311hnvn <zenkai311@gmail.com>
2021-10-29 00:45:06 +0700

    commit home

--- src/home.php
+++ src/home.php
@@ -1,5 +1,5 @@
 <?php
-    include('./reuse/header.php');
+include('./reuse/header.php');
 ?>
 <div class="container-fluid px-0">
     <div class="row p-0 m-0">
@@ -7,7 +7,7 @@
             <nav class="navbar navbar-expand-lg " style="background-color:#ccebff">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="index.php">
-                        <h3 class="text" href="./index.php"><b>TIME CONTROL!<i class="fas fa-clock"></i></b></h3>
+                        <h3 class="text" href="./home.php"><b>Task Management System</b></h3>
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -15,18 +15,33 @@
 
                     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul class="navbar-nav">
-                        <li class="nav-item">
-                                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="categories.php">Categories</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="food.php">Food</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="contact.php">Contact</a>
-                                    </li>
+                            <nav class="navbar navbar-expand-lg ">
+                                <li class="nav-item">
+                                    <a class="nav-link " aria-current="page" href="#">                                    
+                                        <p>Dashboard</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Projects
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a class="dropdown-item" href="#">Add new</a></li>
+                                        <li><a class="dropdown-item" href="#">List</a></li>
+
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">
+                                        Task
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">
+                                        Report
+                                    </a>
+                                </li>
+
                         </ul>
                     </div>
                 </div>
@@ -46,5 +61,5 @@
 
 
 <?php
-    include('./reuse/footer.php')
+include('./reuse/footer.php')
 ?>
