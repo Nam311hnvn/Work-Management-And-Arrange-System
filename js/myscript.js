43ea966ee92cce7fd769a88303f8c9c716c605c4
PhongGT <megayako9@gmail.com>
2021-10-28 15:41:14 +0700

    Xu ly Login

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#Login").click(function(){
+        if ($("#Login_email").var()="") {
+            alert("Bạn chưa nhập Email")
+            
+        };
+        if ("#Login_Password").var()="") {
+            alert("Bạn chưa nhập mật khẩu")
+        }
+        
+    })
+})
--- src/login.php
+++ src/login.php
@@ -1,4 +1,5 @@
 <?php include('./reuse/header.php'); ?>
+
 <div class="container-fluid px-0">
     <section class="vh-100" style="background-color: #eee;">
         <div class="container h-100">
@@ -11,20 +12,20 @@
                                     <form class="mx-4" action="" method="post">
                                         <h2 class="fw-bold text-center">Log in!</h2>
                                         <div class="input-group flex-nowrap py-3">
-                                            <span class="input-group-text" id="addon-wrapping">Email/Username</span>
-                                            <input type="text" class="form-control" placeholder="Enter your Username"
-                                                aria-label="Username" aria-describedby="addon-wrapping">
+                                            <span class="input-group-text" id="addon-wrapping">Email</span>
+                                            <input type="text" class="form-control" placeholder="Enter your Email"
+                                                aria-label="Username" aria-describedby="addon-wrapping" id="Login_email">
                                         </div>
                                         <div class="input-group flex-nowrap pb-3">
                                             <span class="input-group-text" id="addon-wrapping">Password</span>
                                             <input type="text" class="form-control" placeholder="Enter your Password"
-                                                aria-label="Password" aria-describedby="addon-wrapping">
+                                                aria-label="Password" aria-describedby="addon-wrapping" id="Login_Password">
                                         </div>
                                         <div class="d-flex justify-content-start mb-3 mb-lg-4">
                                             <a class="text-decoration-none" href="#">New around here? Sign up</a>
                                         </div>
                                         <div class="d-flex justify-content-start mb-3 mb-lg-4">
-                                            <button type="submit" class="btn btn-primary" name='Login'>Log In</button>
+                                            <button type="submit" class="btn btn-primary" name='Login' id='Login'>Log In</button>
                                         </div>
                                     </form>
                                 </div>
@@ -37,8 +38,4 @@
         </div>
     </section>
 </div>
-
-
-
-
 <?php include('./reuse/footer.php'); ?>
