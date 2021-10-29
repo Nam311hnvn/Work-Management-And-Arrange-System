fede418be5fcc50222834363336be9a245a39d52
HIEPPPP <HIEPPPP@gmail,com>
2021-10-29 23:15:50 +0700

    update view_user

--- src/view_user/project_list.php
+++ src/view_user/project_list.php
@@ -11,41 +11,39 @@
 </div>
 
 <div class="container-fluid">
-    <div class="row pt-2">
-        <div class="col">
-            <div class="mb-2 mb-lg-0 d-flex justify-content-end">
-                <div class="col-md-2 ">
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+	<div class="row pt-2">
+		<div class="col">
+			<div class="mb-2 mb-lg-0 d-flex justify-content-end">
+				<div class="col-md-2 ">
+					<form class="d-flex">
+						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline-success" type="submit">Search</button>
+					</form>
+				</div>
+			</div>
+			<table class="table">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Project</th>
+						<th scope="col">Date Started</th>
+						<th scope="col">Dua Date</th>
+						<th scope="col">Status</th>
+						<th scope="col">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
-</div>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Project</th>
-      <th scope="col">Date Started</th>
-      <th scope="col">Dua Date</th>
-      <th scope="col">Status</th>
-      <th scope="col">Action</th>
-
-
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-	  <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
-
-
-
 <?php include('../reuse/footer.php'); ?>
