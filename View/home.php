228ec91eea3d9e1a09cb1ac60e2e01e451022ff9
Nam311hnvn <zenkai311@gmail.com>
2021-11-05 16:50:09 +0700

    done

--- View/home.php
+++ View/home.php
@@ -31,43 +31,70 @@ if (isset($_SESSION['CurrentUser'])) {
                 </div>
             </div>
         </div>
-
         <div class="row">
             <div class="col-md-8">
                 <table class="table table-responsive">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Project</th>
-                            <th scope="col">Status</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $sql = "SELECT t.*, p.* FROM tb_task t, tb_project p
-                      WHERE p.pj_id = t.pj_id";
-                        $result = mysqli_query($conn, $sql);
-                        $count = 1;
-                        if (mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo '<tr>';
-                                echo '<th scope="row">' . $count++ . '</th>';
-                                echo '<td>' . $row['pj_name'] . '</td>';
-                                echo '<td>' . $row['task_name'] . '</td>';
-                                echo '<a class = "btn btn-primary me-2" href = "edit_task.php?id=' . $row['task_id'] . '">Xem</a>';
-                                echo '<td>';
-                                echo '<tr>';
+                    <table class="table m-0 table-hover">
+                        <!-- <colgroup>
+                            <col width="5%">
+                            <col width="30%">
+                            <col width="15%">
+                            <col width="15%">
+                            <col width="35%">
+                        </colgroup> -->
+                        <thead>
+                            <tr class="text-center">
+                                <th class="col-1 text-center">#</th>
+                                <th class="col-6">Project</th>
+                                <th class="col-5">Status</th>
+                                <th></th>
+                                
+                               
+                            </tr>
+                        </thead>
+                        <tbody class="text-center">
+                            <?php
+                            $sql1 = "SELECT * FROM tb_project ";
+                            $result1 = mysqli_query($conn, $sql1);
+
+
+                            if (mysqli_num_rows($result1) > 0) {
+                                $count = 1;
+                                while ($row = mysqli_fetch_assoc($result1)) { ?>
+                                    <th class="text-center"><?php echo $count++; ?></th>
+                                    <td><?php echo $row['pj_name']; ?></td>
+
+                                    <td><?php
+
+                                        if ($row['pj_status'] == 1) {
+                                            echo '<c class="badge bg-secondary">Pending</c>';
+                                        } else if ($row['pj_status'] == 2) {
+                                            echo '<c class="badge bg-warning">On-Hold</c>';
+                                        } else {
+                                            echo '<span class="badge bg-success">Done</span>';
+                                        }
+                                        ?></td>
+                                    <td><?php echo '<a class = "btn btn-primary" href = "../View/view_project.php?id=' . $row['pj_id'] . '">View</a>'; ?></td>
+
+
+
+                                    </tr>
+
+                            <?php
+                                }
                             }
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                            ?>
+
+                        </tbody>
+                    </table>
             </div>
             <div class="col-md-4">
                 <div class="class col-12">
                     <div class="small-box bg-light shadow-sm border">
                         <div class="inner ps-2">
+                            <!-- câu lệnh in tổng dự án đã tham gia -->
+                            <h3><?php echo $conn->query("SELECT apply_user, COUNT(apply_id) FROM pj_apply GROUP BY apply_user ")->num_rows ?></h3>
+                        
                             <p>Total Projects</p>
                         </div>
                         <div class="icon">
@@ -78,6 +105,7 @@ if (isset($_SESSION['CurrentUser'])) {
                 <div class="class col-12">
                     <div class="small-box bg-light shadow-sm border">
                         <div class="inner ps-2">
+                        <h3><?php echo $conn->query("SELECT user_id, COUNT(task_id) FROM tb_task GROUP BY  user_id ")->num_rows ?></h3>
                             <p>Total Tasks</p>
                         </div>
                         <div class="icon">
