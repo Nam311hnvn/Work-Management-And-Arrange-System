09315b9ae3daf1dc7f4c936c1cb5cefbcfc78628
Nam311hnvn <zenkai311@gmail.com>
2021-11-05 09:31:31 +0700

    fix home

--- View/home.php
+++ View/home.php
@@ -1,25 +1,15 @@
 <?php
+
     //Dịch vụ bảo vệ
     session_start();
     if(isset($_SESSION['CurrentUser'])){
         include('../reuse/header.php');
-        
-?>
-<?php
-if($_SESSION['CurrentLevel'] ==0){
+        include('../reuse/config.php');
 ?>
 <div class="container-fluid">
     <div class="col-12 py-2">
         <div class="card ">
             <div class="card-body ">
-<<<<<<< HEAD
-<<<<<<< HEAD
-                Welcome 
-                <?php echo $_SESSION['CurrentUser']?>!
-=======
-=======
-
->>>>>>> 61f45eb42160b8d6307a9968da4a812ad146803c
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container">
                         <h4 class="navbar-brand" href="#" disabled>Welcome <?php echo $_SESSION['CurrentUser']?>!!</h4>
@@ -31,7 +21,7 @@ if($_SESSION['CurrentLevel'] ==0){
                                             class="fas fa-bell"></i>Notification
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <!-- Làm SQL -->
+                                        <!-- Làm -->
                                         <li><a class="dropdown-item" href="#">Thông tin</a></li>
                                     </ul>
                                 </li>
@@ -40,13 +30,7 @@ if($_SESSION['CurrentLevel'] ==0){
 
                     </div>
                 </nav>
-<<<<<<< HEAD
->>>>>>> abb7c17451b247ce2f79d3c9cb0cf75494851f08
             </div>
-=======
-         
-      </div>
->>>>>>> 61f45eb42160b8d6307a9968da4a812ad146803c
         </div>
     </div>
     
@@ -62,33 +46,12 @@ if($_SESSION['CurrentLevel'] ==0){
                     </tr>
                 </thead>
             </table>
-            <tbody>
-                <?php               
-                include('../reuse/config.php');
-                $sql = "SELECT t.*, p.* FROM tb_task t, tb_project p WHERE p.pj_id = t.pj_id";
-                            
-                        $result = mysqli_query($conn, $sql);
-                        $count = 1;
-                        if (mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo '<tr>';
-                                echo '<th scope="row">' . $count++ . '</th>';
-                                echo '<td>' . $row['pj_name'] . '</td>';
-                                echo '<td>' . $row['pj_status'] . '</td>';    
-                                echo '<a class = "btn btn-primary" href = "delete_task.php?id=' . $row['task_id'] . '">Xóa</a>';                       
-                                echo '<td>';
-                                echo '</tr>';
-                            }
-                        }
-                ?>
-            </tbody>
         </div>
         <div class="col-md-4">
             <div class="class col-12">
                 <div class="small-box bg-light shadow-sm border">
                     <div class="inner ps-2">
-                        <h3><?php echo $conn->query("SELECT Count(user_id)")?></h3>
-                        <p>Total Projects</p>                      
+                        <p>Total Projects</p>
                     </div>
                     <div class="icon">
                         <i class="ps-2 fa fa-layer-group"></i>
@@ -98,7 +61,6 @@ if($_SESSION['CurrentLevel'] ==0){
             <div class="class col-12">
                 <div class="small-box bg-light shadow-sm border">
                     <div class="inner ps-2">
-                        <h3></h3>
                         <p>Total Tasks</p>
                     </div>
                     <div class="icon">
@@ -113,17 +75,5 @@ if($_SESSION['CurrentLevel'] ==0){
 
 include('../reuse/footer.php');
 }else header("Location: ../index.php");
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-?>
-
-=======
 
 ?>
->>>>>>> abb7c17451b247ce2f79d3c9cb0cf75494851f08
-=======
-
-?>
-
->>>>>>> 61f45eb42160b8d6307a9968da4a812ad146803c
